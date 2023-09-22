e8fea5a591407c4c13dd560df87c281b9d070a95
charan1805 <sreecharanmudhiraj@gmail.com>
2023-09-22 16:12:37 +0530

    first class

---
+++ DALTEST/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using DALTEST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTest
+{
+    public partial class UserInfo
+    {
+        public int UserId { get; set; }
+
+        public string Email { get; set; } = null;
+
+        public string Password { get; set; } = null;
+
+        public virtual ICollection<CustLogInfo> CustLogInfos { get; set; } = new List<CustLogInfo>();
+    }
+}
